5afc9ff2c7718ac620396019412f7bf0b36936f6
hindboufeligha <hind.boufeligha@gmail.com>
2026-09-21 00:06:39 +0200

    chore: bootstrap portfolio project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-semibold">Hind Boufeligha</h1>
+    </main>
+  )
+}
+
+export default App
